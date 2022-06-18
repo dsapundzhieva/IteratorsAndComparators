10f8cc944f5d2b1ec3c69b095e6a5ae30e8a9003
Desislava Sapundzhieva <desislava.sapundzhieva@gmail.com>
2022-06-18 12:10:42 +0300

    Add project files.

---
+++ Library/Library.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        public Library(params Book[] books)
+        {
+            this.Books = books.ToList();
+        }
+        public List<Book> Books { get; set; }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            this.Books.Sort();
+            for (int i = 0; i < this.Books.Count; i++)
+            {
+                yield return this.Books[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        //public IEnumerator<Book> GetEnumerator()
+        //{
+        //    return new LibraryIterator(this.Books);
+        //}
+
+        //IEnumerator IEnumerable.GetEnumerator()
+        //{
+        //    return GetEnumerator();
+        //}
+
+        //class LibraryIterator : IEnumerator<Book>
+        //{
+        //    private List<Book> books;
+        //    private int currentÏndex;
+
+        //    public LibraryIterator(List<Book> books)
+        //    {
+        //        this.books = books;
+        //        this.Reset();
+        //    }
+
+        //    public Book Current => this.books[currentÏndex];
+
+        //    object IEnumerator.Current => this.Current;
+
+        //    public void Dispose()
+        //    {
+        //    }
+
+        //    public bool MoveNext()
+        //    {
+        //        this.currentÏndex++;
+        //        return currentÏndex < this.books.Count;
+        //    }
+
+        //    public void Reset()
+        //    {
+        //        this.currentÏndex = -1;
+        //    }
+        //}
+    }
+}
---
+++ Library/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            Book bookTwo = new Book("The Documents in the Case", 2002,
+                "Dorothy Sayers", "Robert Eustace");
+            Book bookThree = new Book("The Documents in the Case (old)", 1930);
+
+            Library libraryOne = new Library();
+            Library libraryTwo = new Library(bookOne, bookTwo, bookThree);
+
+            foreach (var b in libraryTwo)
+            {
+                Console.WriteLine(b.Title);
+            }
+
+            //Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            //Book bookTwo = new Book("The Documents in the Case", 2003, "Dorothy Sayers", "Robert Eustace");
+            //Book bookThree = new Book("The Documents in the Case", 1930);
+            //Book bookFour = new Book("The Documents in the Case", 1900, "Dorothy Sayers", "Robert Eustace");
+            //Book bookFive = new Book("whe Documents in the Case", 1900, "Dorothy Sayers", "Robert Eustace");
+
+            //Library libraryOne = new Library();
+            //Library libraryTwo = new Library(bookOne, bookTwo, bookThree, bookFour, bookFive);
+            //Library library = new Library(bookOne, bookTwo, bookThree);
+
+            //var books = library.ToArray();
+            //Array.Sort(books, new BookComparator());
+
+            //foreach (var book in books)
+            //{
+            //    Console.WriteLine(book);
+            //}
+
+        }
+    }
+}
